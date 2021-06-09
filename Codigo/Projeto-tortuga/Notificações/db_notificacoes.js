a521dfb93d612f20aaa9a57a90ce2d6638662386
Thaís Ferreira da Silva <thais.silva.1336642@sga.pucminas.br>
2021-06-09 19:20:37 -0300

    update de data e checkbox na pagina inicial

--- Codigo/Projeto-tortuga/Notificações/db_notificacoes.js
+++ Codigo/Projeto-tortuga/Notificações/db_notificacoes.js
@@ -3,22 +3,26 @@ const tarefas =
     {
         tarefa: "Estudar",
         prioridade: "Alta",
-        data: "01/06/2021"
+        data: "01/06/2021",
+        status: false
     }, 
     {
         tarefa: "Caminhar",
         prioridade: "Media",
-        data: "02/06/2021"
+        data: "02/06/2021",
+        status: true
     }, 
     {
         tarefa: "Comer",
         prioridade: "Baixa",
-        data: "03/06/2021"
+        data: "03/06/2021",
+        status: false
     }, 
     {
         tarefa: "Andar",
         prioridade: "Alta",
-        data: "04/06/2021"
+        data: "04/06/2021",
+        status: true
     }
 ]
 if(localStorage.getItem("tarefas")== null){
--- Codigo/Projeto-tortuga/Pagina inicial/tarefas.js
+++ Codigo/Projeto-tortuga/Pagina inicial/tarefas.js
@@ -3,11 +3,19 @@ const carregaTarefas = () => {
     console.log(tarefasContainer);
     const tarefasLS = JSON.parse(localStorage.getItem("tarefas")); //pegar o local storage com o id da tarefa
     console.log(tarefasLS);
-
+    
     tarefasLS.map(tarefa => {
-        const button = `<li><button type="button" class="btn btn-success tarefa">${tarefa.tarefa}</button></li>` //configurar o codigo html como se fosse o display da tarefa
-        console.log(button);
-        tarefasContainer.insertAdjacentHTML( 'beforeend', button ); //add a tarefa no html
+        const data = `<button style='width: 150px' type="button" class="btn btn-success tarefa">${tarefa.data}</button>`
+        const button = `<button style='margin-left: 10px' type="button" class="btn btn-success tarefa">${tarefa.tarefa}</button>` //configurar o codigo html como se fosse o display da tarefa
+        let status
+        if(tarefa.status == true){
+            status =`<input type="checkbox" checked>`
+        }else{ 
+            status =`<input type="checkbox">`
+        }
+        const componenteTarefa = `<li class= "tarefa-container">${data} ${button} ${status}</li>`
+        console.log(componenteTarefa);
+        tarefasContainer.insertAdjacentHTML( 'beforeend', componenteTarefa ); //add a tarefa no html
     })
 }
 
